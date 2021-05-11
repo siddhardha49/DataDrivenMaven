91d84d65872d50efc1ac92017007bd90be4f49b3
siddhardha sarikonda <siddhu>
2021-05-11 19:08:32 -0400

    Initial Commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	@BeforeSuite
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+	}
+	
+	@Test
+	public void doLogin() {
+		driver.get("http://www.gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("siarikondasiddhu");
+	}
+
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+}
